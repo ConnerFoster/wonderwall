dfd46d66f139cc3a93e4dc1a2a9932dcf2bd3ee3
Conner Foster <connerf99@gmail.com>
2023-02-07 22:53:26 -0500

    Completed controller functions for posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,28 +1,59 @@
 const asyncHandler = require('express-async-handler')
 
+const Post = require('../models/postModel')
+
 //Get ALL posts
 const getPosts = asyncHandler(async (req, res) => {
-  res.status(200).json({message: 'get posts'})
+  const posts = await Post.find()
+
+  res.status(200).json(posts)
 })
 
 //create a post
 const setPost = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  if (!req.body.song) {
     res.status(400)
-    throw new Error('Please add a text field')
+    throw new Error('Please add a song')
   }
 
-  res.status(200).json({message: 'set post'})
+  const post = await Post.create({
+    text: req.body.text || '',
+    song: req.body.song,
+    likes: 0,
+    comments: [],
+  })
+
+  res.status(200).json(post)
 })
 
 //update a post
 const updatePost = asyncHandler(async (req, res) => {
-  res.status(200).json({message: 'update a post'})
+  const post = await Post.findById(req.params.id)
+
+  if (!post) {
+    res.status(400)
+    throw new Error('Post not found')
+  }
+
+  const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+
+  res.status(200).json(updatedPost)
 })
 
 //delete a post
 const deletePost = asyncHandler(async (req, res) => {
-  res.status(200).json({message: 'delete post'})
+  const post = await Post.findById(req.params.id)
+
+  if (!post) {
+    res.status(400)
+    throw new Error('Post not found')
+  }
+
+  await post.remove()
+
+  res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
