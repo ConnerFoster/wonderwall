ad760fe2c7fc312529fef46c29987d963f232513
Conner Foster <connerf99@gmail.com>
2023-02-08 14:59:34 -0500

    complete methods to register, login, get user data, and create jwt

---
+++ backend/controllers/userController.js
@@ -0,0 +1,79 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+//Register a new user
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    res.status(400)
+    throw new Error('Both fields required')
+  }
+
+  //check if username is already registered
+  const userExists = await User.findOne({ username })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('Username already registered')
+  }
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPW = await bcrypt.hash(password, salt)
+
+  const user = await User.create({
+    username,
+    password: hashedPW,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      username: user.username,
+      token: createJWT(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+
+//Authenticate user
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, password } = req.body
+
+  const user = await User.findOne({ username })
+
+  //check that user is registered and that they have entered correct pw
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      username: user.username,
+      token: createJWT(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid username or password')
+  }
+})
+
+//Get user's data
+const getUser = asyncHandler(async (req, res) => {
+  res.json({ message: 'Get me' })
+})
+
+//Create JWT
+const createJWT = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getUser,
+}
