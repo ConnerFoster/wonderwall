f2936a82fed51d18c5193efcb2657d287365393a
Conner Foster <connerf99@gmail.com>
2023-02-08 17:13:07 -0500

    add error checking for delete and update functions, making sure that only owner of posts can update and delete them

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,10 +1,18 @@
 const asyncHandler = require('express-async-handler')
 
 const Post = require('../models/postModel')
+const User = require('../models/userModel')
 
 //Get ALL posts
 const getPosts = asyncHandler(async (req, res) => {
   const posts = await Post.find()
+
+  res.status(200).json(posts)
+})
+
+//get current logged in user's posts ONLY
+const getUserPosts = asyncHandler(async (req, res) => {
+  const posts = await Post.find({ user: req.user.id })
 
   res.status(200).json(posts)
 })
@@ -17,7 +25,8 @@ const setPost = asyncHandler(async (req, res) => {
   }
 
   const post = await Post.create({
-    text: req.body.text || '',
+    text: req.body.text,
+    user: req.user.id,
     song: req.body.song,
     likes: 0,
     comments: [],
@@ -33,6 +42,19 @@ const updatePost = asyncHandler(async (req, res) => {
   if (!post) {
     res.status(400)
     throw new Error('Post not found')
+  }
+
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('user not found')
+  }
+
+  //Check that logged in user is the author of the post
+  if (post.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error('User is not the author of this post')
   }
 
   const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
@@ -51,13 +73,27 @@ const deletePost = asyncHandler(async (req, res) => {
     throw new Error('Post not found')
   }
 
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('user not found')
+  }
+
+  //Check that logged in user is the author of the post
+  if (post.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error('User is not the author of this post')
+  }
+
   await post.remove()
 
-  res.status(200).json({id: req.params.id})
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
   getPosts,
+  getUserPosts,
   setPost,
   updatePost,
   deletePost,
