5500e1eeb93559b320109a2fbe5464deea23e30c
Conner Foster <connerf99@gmail.com>
2023-02-08 21:46:28 -0500

    Add login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { BsFillMusicPlayerFill } from 'react-icons/bs'
 
 function Login() {
-  return <div>Login</div>
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  })
+
+  const { username, password } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className='h-screen text-[#ddd] text-center flex flex-col gap-5 justify-center'>
+      <section className='flex flex-col items-center gap-3'>
+        <BsFillMusicPlayerFill className='w-12 h-12' />
+        <h1 className='text-2xl'>Welcome back! &#128075;</h1>
+        <h3>Please enter your login credentials below</h3>
+      </section>
+
+      <section>
+        <form onSubmit={onSubmit}>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            value={username}
+            placeholder='Username'
+            className='inputs'
+            onChange={onChange}
+          />
+          <input
+            type='text'
+            id='password'
+            name='password'
+            value={password}
+            placeholder='Password'
+            className='inputs'
+            onChange={onChange}
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </section>
+    </div>
+  )
 }
 
 export default Login
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { BsFillMusicPlayerFill } from 'react-icons/bs'
 
 function Register() {
-  return <div>Register</div>
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  })
+
+  const { username, password } = formData
+
+  const onChange = () => {}
+
+  const onSubmit = () => {}
+
+  return (
+    <div className='h-screen text-[#ddd] text-center flex flex-col gap-5 justify-center'>
+      <section className='flex flex-col items-center gap-3'>
+        <BsFillMusicPlayerFill className='w-12 h-12' />
+        <h1 className='text-2xl'>Hello, there!</h1>
+        <h3>Register for an account below</h3>
+      </section>
+
+      <section>
+        <form onSubmit={onSubmit}>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            value={username}
+            placeholder='Username'
+            className='inputs'
+            onChange={onChange}
+          />
+          <input
+            type='text'
+            id='password'
+            name='password'
+            value={password}
+            placeholder='Password'
+            className='inputs'
+            onChange={onChange}
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </section>
+    </div>
+  )
 }
 
 export default Register
