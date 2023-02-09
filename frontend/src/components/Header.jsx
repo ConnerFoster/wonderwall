3010b623898f4e36b7d4772b52138316328ae0f0
Conner Foster <connerf99@gmail.com>
2023-02-09 18:49:38 -0500

    add logout button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { RiHome2Fill, RiAddBoxLine } from 'react-icons/ri'
 import { HiUserCircle } from 'react-icons/hi'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
 
 function Header() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
+
   return (
     <header className='bg-[#232428] text-[#ddd] sticky top-0 py-5 px-10 flex justify-evenly items-center xl:items-center'>
       <div>
@@ -22,6 +34,7 @@ function Header() {
         <Link to='/profile'>
           <HiUserCircle className='icons' />
         </Link>
+        <button onClick={onLogout}>Logout</button>
       </div>
     </header>
   )
