a7d51c3ea5c52bb5d962db0c6e07de1d970c69a6
Conner Foster <connerf99@gmail.com>
2023-02-09 14:50:21 -0500

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const colors = require('colors')
 const connectToDatabase = require('./config/db')
+const cors = require('cors')
 
 connectToDatabase()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/posts', require('./routes/postRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
