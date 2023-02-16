e943c2a38e4d0cc2ee076964772e0ce619e8b85c
Conner Foster <connerf99@gmail.com>
2023-02-16 14:07:01 -0500

    update state object to include song preview url

--- frontend/src/pages/NewPost.jsx
+++ frontend/src/pages/NewPost.jsx
@@ -7,7 +7,6 @@ import Header from '../components/Header'
 import SongResults from '../components/SongResults'
 import PostForm from '../components/PostForm'
 import { submitPost } from '../features/posts/postSlice'
-import searchService from '../features/search/searchService'
 
 function NewPost() {
   const dispatch = useDispatch()
@@ -18,6 +17,7 @@ function NewPost() {
     songTitle: '',
     songArtist: '',
     songImgUrl: '',
+    songPreviewUrl: '',
   })
   const [results, setResults] = useState([])
 
@@ -26,7 +26,6 @@ function NewPost() {
       query: query,
     }
     axios.post('http://localhost:5000/api/search', body).then((res) => {
-      console.log(res.data)
       setResults(res.data.items)
     })
   }
@@ -37,6 +36,7 @@ function NewPost() {
       songTitle: songObj.songTitle,
       songArtist: songObj.songArtist,
       songImgUrl: songObj.songImgUrl,
+      songPreviewUrl: songObj.songPreviewUrl,
     }))
   }
 
@@ -48,7 +48,8 @@ function NewPost() {
     if (
       song.songTitle === '' ||
       song.songArtist === '' ||
-      song.songImgUrl === ''
+      song.songImgUrl === '' ||
+      song.songPreviewUrl === ''
     ) {
       return
     }
@@ -57,6 +58,7 @@ function NewPost() {
       songTitle: song.songTitle,
       songArtist: song.songArtist,
       songImgUrl: song.songImgUrl,
+      songPreviewUrl: song.songPreviewUrl,
     }
 
     dispatch(submitPost(data))
@@ -65,6 +67,7 @@ function NewPost() {
       songTitle: '',
       songArtist: '',
       songImgUrl: '',
+      songPreviewUrl: '',
     }
     updateSong(reset)
     updateText('')
@@ -74,7 +77,7 @@ function NewPost() {
 
   return (
     <div className='h-screen text-gray-500 text-center'>
-      <Header />
+      <Header page='New Post' />
       <SongSearch fetchSongs={fetchSongs} />
       <div className='flex justify-evenly mt-5 max-w-7xl items-center mx-auto'>
         <SongResults searchRes={results} updateSong={updateSong} />
@@ -82,6 +85,8 @@ function NewPost() {
           updateText={updateText}
           text={text}
           handleSubmit={handleSubmit}
+          songImg={song.songImgUrl}
+          updateSong={updateSong}
         />
       </div>
     </div>
