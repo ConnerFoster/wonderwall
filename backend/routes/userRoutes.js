5dee2040986f4e0f1f7956afd3ef74a72ef65066
Conner Foster <connerf99@gmail.com>
2023-02-17 00:19:51 -0500

    Fixed issue where userRoute was not getting protectRoute middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,14 +4,13 @@ const {
   registerUser,
   loginUser,
   getUser,
-  storeImage,
   updateUser,
 } = require('../controllers/userController')
 const { protectRoute } = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.put('/', updateUser, protectRoute)
+router.put('/', protectRoute, updateUser)
 //change this to /u/username later
 router.get('/me', protectRoute, getUser)
 
