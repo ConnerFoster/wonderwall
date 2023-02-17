c9636a9244f2dcfce0b33a0cc6b444147a39a80f
Conner Foster <connerf99@gmail.com>
2023-02-17 00:21:11 -0500

    Added Edit Profile page with form to update user info

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { HiUserCircle } from 'react-icons/hi'
 import { FaSave } from 'react-icons/fa'
+import { IoArrowBackOutline } from 'react-icons/io5'
 import Header from '../components/Header'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { getUser, updateUser } from '../features/user/userSlice'
+import { Link } from 'react-router-dom'
 
 function EditProfile() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
   const { userProfile } = useSelector((state) => state.user)
+  console.log(userProfile)
 
   const [image, setImage] = useState(userProfile.profilePhoto || null)
   const [updateInfo, setUpdateInfo] = useState({
@@ -15,6 +25,10 @@ function EditProfile() {
 
   const { bio, displayName } = updateInfo
 
+  useEffect(() => {
+    dispatch(getUser())
+  }, [dispatch])
+
   const onChange = (e) => {
     setUpdateInfo((prevState) => ({
       ...prevState,
@@ -22,17 +36,66 @@ function EditProfile() {
     }))
   }
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault()
+
+    let imageUrl = ''
+
+    if (image && image != userProfile.profilePhoto) {
+      const instance = axios.create()
+
+      const formData = new FormData()
+      formData.append('file', image)
+      formData.append('upload_preset', 'ww_preset')
+      const res = await instance.post(
+        'https://api.cloudinary.com/v1_1/dcwvy5ykr/image/upload',
+        formData
+      )
+
+      imageUrl = res.data.url
+    }
+    const data = {
+      profilePhoto: imageUrl,
+      bio: bio,
+      displayName: displayName,
+    }
+
+    console.log(data)
+
+    dispatch(updateUser(data))
+    navigate('/profile')
+  }
+
+  const getIcon = () => {
+    if (image) {
+      if (typeof image != 'string') {
+        return (
+          <img
+            src={URL.createObjectURL(image)}
+            className='w-24 rounded-full mb-2'
+          />
+        )
+      } else {
+        return <img src={image} className='w-24 rounded-full mb-2' />
+      }
+    } else {
+      return <HiUserCircle size={120} color='#ddd' />
+    }
   }
 
   return (
     <div>
-      <Header />
+      <Header page='Edit Profile' />
+      <Link to='/profile'>
+        <div className='mt-10 mx-10'>
+          <IoArrowBackOutline color='#ddd' size={40} />
+        </div>
+      </Link>
+
       <div className='text-[#ddd] flex flex-col gap-5 justify-center items-center mt-[10vh]'>
         <section className='flex flex-col items-center'>
-          <HiUserCircle size={70} color='#ddd' />
-          <label className='text-[#5865f2]/70 font-semibold'>
+          {getIcon()}
+          <label className='text-[#5865f2]/70 font-semibold text-lg cursor-pointer'>
             <input
               type='file'
               name='profilePhoto'
@@ -53,7 +116,7 @@ function EditProfile() {
               name='displayName'
               value={displayName}
               placeholder='Display Name'
-              className='inputs'
+              className='inputs w-96'
               onChange={onChange}
             />
             <textarea
@@ -61,12 +124,12 @@ function EditProfile() {
               name='bio'
               value={bio}
               placeholder='Bio'
-              className='bg-[#232428] rounded-2xl w-64 h-32 p-3 resize-none'
+              className='bg-[#232428] rounded-2xl w-96 h-32 p-3 resize-none'
               onChange={onChange}
             />
             <button
               type='submit'
-              className='flex justify-center items-center gap-1 bg-[#5865f2]/90 hover:bg-[#5865f2] rounded-full py-2 text-white block w-[100%]'>
+              className='flex justify-center items-center gap-1 bg-[#5865f2]/90 hover:bg-[#5865f2] rounded-full py-2 text-white block w-[95%]'>
               <FaSave size={15} />
               Save
             </button>
