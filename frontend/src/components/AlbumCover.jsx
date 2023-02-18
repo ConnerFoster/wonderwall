9181733f940aa2d26dbf39c80daecbaf63da6583
Conner Foster <connerf99@gmail.com>
2023-02-18 17:31:03 -0500

    Add mask to image so text stands out on light backgrounds

--- frontend/src/components/AlbumCover.jsx
+++ frontend/src/components/AlbumCover.jsx
@@ -9,13 +9,13 @@ function AlbumCover(props) {
       <div className='relative bottom-[9rem] left-[20%]'>
         <img
           src={props.img}
-          className='w-72 rounded-md absolute cursor-pointer'
+          className='w-72 rounded-md absolute cursor-pointer darken'
           onClick={() => handleAudio(props.preview)}
         />
         <h1 className='text-[#ddd] font-bold text-[#ddd] absolute top-[15rem] left-[2%] '>
           {props.title}
         </h1>
-        <h1 className='absolute text-[#ddd] top-[16rem] left-[2%] '>
+        <h1 className='absolute  text-[#ddd] top-[16rem] left-[2%] '>
           {props.artist}
         </h1>
       </div>
