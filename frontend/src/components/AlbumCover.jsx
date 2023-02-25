3d92987d7d8a5579820e19af94586e13ec244ddc
Conner Foster <connerf99@gmail.com>
2023-02-24 21:50:11 -0500

    Adjust image placement

--- frontend/src/components/AlbumCover.jsx
+++ frontend/src/components/AlbumCover.jsx
@@ -10,16 +10,16 @@ function AlbumCover(props) {
   }
   return (
     <div>
-      <div className='relative bottom-[9rem] left-[20%]'>
+      <div className='relative bottom-[10rem] left-[18%]'>
         <img
           src={props.img}
-          className='w-72 rounded-md absolute cursor-pointer darken'
+          className='w-80 rounded-xl absolute cursor-pointer darken'
           onClick={() => handleAudio(props.preview)}
         />
-        <h1 className='text-[#ddd] break-words font-bold text-[#ddd] absolute top-[15rem] left-[2%] '>
+        <h1 className='text-gray-300/90 break-words font-semibold text-[#ddd] absolute top-[16.5rem] left-[2%] '>
           {props.title.length > 34 ? breakLongText(props.title) : props.title}
         </h1>
-        <h1 className='absolute  text-[#ddd] top-[16rem] left-[2%] '>
+        <h1 className='absolute text-gray-300/90 top-[17.5rem] left-[2%] '>
           {props.artist.length > 34
             ? breakLongText(props.artist)
             : props.artist}
